14edff6ce2aaf735007504231b7213a1bcd6067a
Don Button <donpublic@digitalgizmo.com>
2016-12-12 10:24:14 -0500

    start on team story list
    detail has template not found error

--- impressions/stories/urls.py
+++ impressions/stories/urls.py
@@ -4,6 +4,13 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.StoryListView.as_view(), name='story_list'),
-    url(r'^(?P<slug>\S+)/(?P<chapter_num>\d+)/$', views.ChapterDetailView.as_view(), name='chapter_detail'),
+    url(r'^team/$', 
+        views.StoryListView.as_view(template_name="stories/team_story_list.html"), 
+    	name='team_story_list'),
+    url(r'^team/(?P<slug>\S+)/$', 
+        views.StoryDetailView.as_view(template_name="stories/team_story_detail.html"), 
+    	name='team_story_detail'),
+    url(r'^(?P<slug>\S+)/(?P<chapter_num>\d+)/$', views.ChapterDetailView.as_view(), 
+    	name='chapter_detail'),
     url(r'^(?P<slug>\S+)/$', views.StoryDetailView.as_view(), name='story_detail'),
 ]
