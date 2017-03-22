7afd7780954fae01b5e48a1d177f7b6587fd798f
Don Button <donpublic@digitalgizmo.com>
2017-03-22 12:37:29 -0400

    coming soon js

--- impressions/design/coming-soon/js/intro-slideshow.js
+++ impressions/design/coming-soon/js/intro-slideshow.js
@@ -1,14 +1,51 @@
 $(document).ready(function(){
 
-	$("#intro_slideshow > div:gt(0)").hide();
+    var slides = $(".mySlides");
+    var dots = $(".dot");
+    // var slides = document.getElementsByClassName("mySlides");
+    // var dots = document.getElementsByClassName("dot");
+	var timer;
+	var slideIndex = 0;
+	showSlides();
 
-	setInterval(function() { 
-	  $('#intro_slideshow > div:first')
-	    .fadeOut(1000)
-	    .next()
-	    .fadeIn(1000)
-	    .end()
-	    .appendTo('#intro_slideshow');
-	},  3000);
 
-}); // end doc ready
+	function showSlides() {
+	    slideIndex++;	
+		showSingle(slideIndex);	    
+	    // set the timer
+	    timer = setTimeout(showSlides, 4000); // Change image every 2 seconds
+	}
+
+	function showSingle(n) {
+		var i;
+	    // restart count if at end
+		if (n > slides.length) {slideIndex = 1} 
+		// if 0 (how?) go to end
+		if (n < 1) {slideIndex = slides.length}
+		// hide all slides
+		for (i = 0; i < slides.length; i++) {
+		  slides[i].style.display = "none"; 
+		}
+	    // clear the dot highlights  
+		for (i = 0; i < dots.length; i++) {
+		  dots[i].className = dots[i].className.replace(" active", "");
+		}
+	    // show the current slide
+		slides[slideIndex-1].style.display = "block"; 
+	    // show the current dot
+		dots[slideIndex-1].className += " active";
+	}
+
+	// set event listener for dots
+	$('.dot').click(function(event){
+		// event.preventDefault();
+	    var slide_num = $(event.target).attr('name');
+	    // Stop timer
+	    clearTimeout(timer);
+	    // set index
+	    slideIndex = slide_num;
+	    // show single
+	    showSingle(slide_num);
+	});
+
+});
