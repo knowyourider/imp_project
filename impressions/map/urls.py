c7d033b48696f94ae2d261e5aaa77b8ca19f4807
Don Button <donpublic@digitalgizmo.com>
2017-05-27 14:13:30 -0400

    map intro template

--- impressions/map/urls.py
+++ impressions/map/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    url(r'^intro/$', TemplateView.as_view(template_name="map/map_intro.html"), name='map_intro'),
     url(r'^sites/(?P<slug>\S+)/$', views.layer_sites, name='site_list'),    
     url(r'^deeper/(?P<slug>\S+)/$', views.MapDeeperView.as_view(), name='map_deeper'),    
     # url(r'^about/(?P<layer_index>[0-9]+)/$', views.MapAboutView.as_view(), name='map_about'),    
