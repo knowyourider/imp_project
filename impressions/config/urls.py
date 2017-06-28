6944f0a373c78f2e176d93a874b5742a365bce0b
Don Button <donpublic@digitalgizmo.com>
2017-06-28 09:12:19 -0400

    supress admin on prod

--- impressions/config/urls.py
+++ impressions/config/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-# from core import views
+from django.conf import settings
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # url(r'^admin/', admin.site.urls),
     url(r'^ancillary/', include('ancillary.urls', namespace='ancillary')),
     url(r'^', include('core.urls', namespace='core')),
     url(r'^map/', include('map.urls', namespace='map')),
@@ -11,5 +11,7 @@ urlpatterns = [
     url(r'^stories/', include('stories.urls', namespace='stories')),
     url(r'^supporting/', include('supporting.urls', namespace='supporting')),
     url(r'^themes/', include('themes.urls', namespace='themes')),
-    # url(r'^team/$', views.TeamHomeTemplateView.as_view(), name='team_home'),
 ]
+
+if settings.DEBUG:
+	urlpatterns.append( url(r'^admin/', admin.site.urls))
