50622d23e654c00deaa27c92c7e00c21e7032c86
julietanna <juliet@digitalgizmo.com>
2017-11-09 13:29:57 -0500

    made first drafts of teachers and further, edited footer in base to link to these pages and added contact us

--- impressions/ancillary/urls.py
+++ impressions/ancillary/urls.py
@@ -5,6 +5,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     url(r'^about/$', TemplateView.as_view(template_name="ancillary/about.html"), name='about'),
     url(r'^teachers/$', TemplateView.as_view(template_name="ancillary/teachers.html"), name='teachers'),
+    url(r'^further/$', TemplateView.as_view(template_name="ancillary/further.html"), name='further'),
     # url(r'^sites/(?P<slug>\S+)/$', views.layer_sites, name='site_list'),    
     # url(r'^deeper/(?P<slug>\S+)/$', views.MapDeeperView.as_view(), name='map_deeper'),    
     # url(r'^about/(?P<slug>\S+)/$', views.MapAboutView.as_view(), name='about_detail'),    
